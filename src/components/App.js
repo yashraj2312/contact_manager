9d0e9f974b70530aab08bc69c0acb0c713c3de55
yashraj2312 <yashrajsrivastava2312@gmail.com>
2022-09-22 09:08:48 +0530

    feat:contact list created

--- src/components/App.js
+++ src/components/App.js
@@ -6,11 +6,27 @@ import ContactList from './ContactList'
 
 
 const App =()=>{
+
+  // array list of contacts
+  const listOfContacts = [      
+    {
+      id:1,
+      name:"yash",
+      email:"abc@gmail.com"
+
+    },
+    {
+      id:2,
+      name:"raj",
+      email:"xyz@gmail.com"
+    }
+  ]
   return(
   <div className='ui container'>
     <Header/>
     <AddContact/>
-    <ContactList/>
+    {/* props -- passing array of contacts i.e listOfContacts as a property (contacts) ---props are basically used pass properties of parent to child*/}
+    <ContactList contacts={listOfContacts}/>
   </div>
   )
 }
--- src/components/ContactList.jsx
+++ src/components/ContactList.jsx
@@ -1,8 +1,22 @@
 import React from 'react'
 
-const ContactList =()=>{
+// accesing props created in App js
+const ContactList =(props)=>{
+    console.log(props);
+
+    const showContactList = props.contacts.map((contact)=>{
+        return (
+            <div className='item'>
+                <div className='content'>
+                    <div className='header'>{contact.name}</div>
+                    <div>{contact.email}</div>
+                </div>
+                <i className='trash alternate outline icon'></i>
+            </div>
+        )
+    })
     return (
-        <div className='ui celled list'>Contact List</div>
+        <div className='ui celled list'>{showContactList}</div>
     )
 }
 
